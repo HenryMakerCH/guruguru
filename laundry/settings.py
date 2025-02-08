7704db291f2a36bf5056771234fca4b4d258a88a
HenryMakerCH <89520883+HenryMakerCH@users.noreply.github.com>
2025-02-09 00:49:17 +0800

    Update settings.py

--- laundry/settings.py
+++ laundry/settings.py
@@ -4,27 +4,27 @@
 
 
 # UserId 自行获取
-userId = 12149239
+userId = 11451419
 
 # 上传的乐曲成绩
 # 此成绩将覆盖原有成绩
 
 music_data = ({
-    "musicId": 299,
-    "level": 4,#Basic=0,Re:Master=4
-    "playCount": 9,
-    "achievement": 999289,
+    "musicId": 11451,
+    "level": 3,#Basic=0,Re:Master=4
+    "playCount": 1,
+    "achievement": 1010000,
     "comboStatus": 0,
     "syncStatus": 0,
-    "deluxscoreMax": 2397,
-    "scoreRank": 11,#D=0,SSS+=13
+    "deluxscoreMax": 2000,
+    "scoreRank": 13,#D=0,SSS+=13
     "extNum1": 0
 })
 
 # 机厅信息
 
-regionId = 5
-regionName = "安徽"
-placeId = 3023
-placeName = "星奇多合肥瑶海龙湖天街店"
-clientId = "A63E01E6873"
+regionId = 
+regionName = ""
+placeId = 
+placeName = ""
+clientId = "A63E"
