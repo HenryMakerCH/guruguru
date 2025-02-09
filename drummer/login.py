dd6d6c1db62619c3cec7f8179277cd3335502c85
HenryMakerCH <89520883+HenryMakerCH@users.noreply.github.com>
2025-02-09 21:02:58 +0800

    Add files via upload

---
+++ drummer/login.py
@@ -0,0 +1,26 @@
+import json
+import pytz
+
+from sdgb import sdgb_api
+from datetime import datetime
+
+from musicdata import userId
+from settings import regionId, clientId, placeId
+
+def login(timestamp):
+    data = json.dumps({
+        "userId": userId,
+        "accessCode": "",
+        "regionId": regionId,
+        "placeId": placeId,
+        "clientId": clientId,
+        "dateTime": timestamp,
+        "isContinue": False,
+        "genericFlag": 0,
+    })
+
+    login_result = sdgb_api(data, "UserLoginApi", userId)
+    return login_result
+
+if __name__ == "__main__":
+    print(login(int(input())))
---
+++ drummer/settings.py
@@ -0,0 +1,30 @@
+# This file contains the config of the whole project.
+# DO NOT share your crtical info to others.
+# Change the file name from .settings.py to settings.py.
+
+
+# UserId 自行获取
+#userId = 12149239
+
+# 上传的乐曲成绩
+# 此成绩将覆盖原有成绩
+
+music_data = ({
+    "musicId": 11311,
+    "level": 2,#Basic=0,Re:Master=4
+    "playCount": 5,
+    "achievement": 1008234,
+    "comboStatus": 2,
+    "syncStatus": 0,
+    "deluxscoreMax": 1949,
+    "scoreRank": 13,#D=0,SSS+=13
+    "extNum1": 0
+})
+
+# 机厅信息
+
+regionId = 5
+regionName = "安徽"
+placeId = 3023
+placeName = "星奇多合肥瑶海龙湖天街店"
+clientId = "A63E01E6873"
