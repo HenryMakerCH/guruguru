d07e3bd1c0aaab022d5a73c2a1cce1984fa4f1e4
HenryMakerCH <89520883+HenryMakerCH@users.noreply.github.com>
2025-02-09 20:39:43 +0800

    Delete laundry directory

--- laundry/README.md
+++
@@ -1,2 +0,0 @@
-For the first time please install requirements using `pip install -r requirements.txt`
-#Usage
--- laundry/prev.py
+++
@@ -1,7 +0,0 @@
-import json
-
-from sdgb import sdgb_api
-from preview import preview
-
-if __name__ == "__main__":
-    print(preview(int(input())))
