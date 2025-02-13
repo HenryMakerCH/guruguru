04cd4134d6dc00ee521a3eb5fc9bea853f2a8817
HenryMakerCH <89520883+HenryMakerCH@users.noreply.github.com>
2025-02-13 21:56:27 +0800

    Update settings.py

--- drummer/settings.py
+++ drummer/settings.py
@@ -1,7 +1,7 @@
 # 机厅信息
 
-regionId = 5
-regionName = "安徽"
-placeId = 3023
-placeName = "星奇多合肥瑶海龙湖天街店"
-clientId = "A63E01E6873"
+regionId = 
+regionName = ""
+placeId = 
+placeName = ""
+clientId = "A63E"
